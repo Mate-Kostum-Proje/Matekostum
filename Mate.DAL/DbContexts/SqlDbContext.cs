229822a0b2d8ca24988b39382206f37f8eeb86d9
idilerdogan <idilerdogan@sabanciuniv.edu>
2024-12-15 00:19:28 +0300

    tubaya göre db ayarları değştirildi

--- Mate.DAL/DbContexts/SqlDbContext.cs
+++ Mate.DAL/DbContexts/SqlDbContext.cs
@@ -4,46 +4,46 @@ using Microsoft.EntityFrameworkCore;
 
 namespace Mate.DAL.DbContexts
 {
-    public class SqlDbContext : DbContext
-    {
-        public DbSet<Basket> Baskets { get; set; }
-        public DbSet<BasketDetail> BasketDetails { get; set; }
+	public class SqlDbContext : DbContext
+	{
+		public DbSet<Basket> Baskets { get; set; }
+		public DbSet<BasketDetail> BasketDetails { get; set; }
 
-        public DbSet<CommUser> CommUsers { get; set; }
-        public DbSet<FotoGalery> FotoGaleries { get; set; }
-        public DbSet<Order> Orders { get; set; }
-        public DbSet<OrderDetail> OrdersDetail { get; set; }
+		public DbSet<CommUser> CommUsers { get; set; }
+		public DbSet<FotoGalery> FotoGaleries { get; set; }
+		public DbSet<Order> Orders { get; set; }
+		public DbSet<OrderDetail> OrdersDetail { get; set; }
 
-        public DbSet<OrderSituation> OrdersSituation { get; set; }
-        public DbSet<Product> Products { get; set; }
-        public DbSet<ProductCategory> ProductCategories { get; set; }
-        public DbSet<ProductRegion> ProductRegions { get; set; }
-        public DbSet<ProductSubRegion> ProductSubRegions { get; set; }
-        public DbSet<SiteCommInfo> SiteCommInfos { get; set; }
-        public DbSet<UserInfo> UserInfos { get; set; }
-        public DbSet<Role> Roles { get; set; }
-
-
+		public DbSet<OrderSituation> OrdersSituation { get; set; }
+		public DbSet<Product> Products { get; set; }
+		public DbSet<ProductCategory> ProductCategories { get; set; }
+		public DbSet<ProductRegion> ProductRegions { get; set; }
+		public DbSet<ProductSubRegion> ProductSubRegions { get; set; }
+		public DbSet<SiteCommInfo> SiteCommInfos { get; set; }
+		public DbSet<UserInfo> UserInfos { get; set; }
+		public DbSet<Role> Roles { get; set; }
 
 
-        public SqlDbContext()
-        {
 
-        }
 
-        public SqlDbContext(DbContextOptions<SqlDbContext> options) : base(options)
-        {
+		public SqlDbContext()
+		{
 
-        }
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        {
-            //base.OnConfiguring(optionsBuilder);
-            optionsBuilder.UseSqlServer("server=IDILERDOGAN\\MSSQLSERVER01;Database=MateKostum;Trusted_Connection=true;TrustServerCertificate=true");
-        }
-        protected override void OnModelCreating(ModelBuilder modelBuilder)
-        {
-            modelBuilder.ApplyConfigurationsFromAssembly(typeof(BaseConfig<>).Assembly);
-        }
-    }
+		}
+
+		public SqlDbContext(DbContextOptions<SqlDbContext> options) : base(options)
+		{
+
+		}
+		protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+		{
+			//base.OnConfiguring(optionsBuilder);
+			optionsBuilder.UseSqlServer("server=.;Database=MateKostum;Trusted_Connection=true;TrustServerCertificate=true");
+		}
+		protected override void OnModelCreating(ModelBuilder modelBuilder)
+		{
+			modelBuilder.ApplyConfigurationsFromAssembly(typeof(BaseConfig<>).Assembly);
+		}
+	}
 }
 
