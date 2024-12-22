75929798d29b0754cd8c37eb0b3419b2edd0c80f
idilerdogan <idilerdogan@sabanciuniv.edu>
2024-12-22 20:45:59 +0300

    icon eklenip actionlar halledildi

--- Mate.BL/Abstract/IOrderManager.cs
+++ Mate.BL/Abstract/IOrderManager.cs
@@ -4,6 +4,7 @@ namespace Mate.BL.Abstract
 {
     public interface IOrderManager : IManager<Order>
     {
+
         public bool CanPlaceOrder(string userId, List<OrderDetail> orderDetails, List<ProductSize> productSizes);
         public bool PlaceOrder(string userId, List<OrderDetail> orderDetails, List<ProductSize> productSizes, List<Product> products);
         public void Update(Product product);
